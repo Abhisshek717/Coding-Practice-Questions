8a72878c13253a0bc2bf004fc53e6935318aff5f
Abhishek <46296076+Abhisshek717@users.noreply.github.com>
2025-11-05 02:55:24 +0530

    Add Exercise 5: Print numbers from 10 to 1

---
+++ Loops/Easy/Excersice5.c
@@ -0,0 +1,16 @@
+// Excersice 4: Print numbers from 10 to 1 (use while loop)
+
+#include <stdio.h>
+
+int main() {
+    
+    int i = 10;
+    
+    while(i >= 1)
+    {
+        printf("%d\n",i);
+        i--;
+    }
+
+    return 0;
+}
