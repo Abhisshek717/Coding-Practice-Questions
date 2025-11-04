296349ccc337dcd8bb1f5dcfad2a318ea93eb518
Abhishek <46296076+Abhisshek717@users.noreply.github.com>
2025-11-05 02:52:57 +0530

    Add program to sum first 10 natural numbers
    This program calculates the sum of the first 10 natural numbers and prints the result.

---
+++ Loops/Easy/Excersice3.c
@@ -0,0 +1,17 @@
+// Excersice 3: Sum of first 10 natural numbers
+
+#include <stdio.h>
+
+int main() {
+    
+    int sum = 0;
+    
+    for(int i=1;i<=10;i++)
+    {
+        sum += i;    
+    }
+    
+    printf("sum of first 10 natural numbers: %d", sum);
+
+    return 0;
+}
