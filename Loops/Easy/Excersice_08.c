3f9308bd40705430f44a1b52583c3a7b9ca934cb
Abhishek <46296076+Abhisshek717@users.noreply.github.com>
2026-01-03 16:36:26 +0530

    Add output comments to Excersice_08.c
    Added output comments to clarify expected results.

---
+++ Loops/Easy/Excersice_08.c
@@ -0,0 +1,55 @@
+// Print numbers from 1 to N, but skip multiples of 3.
+
+#include <stdio.h>
+
+int main() {
+    
+    int n = 100;
+
+    for(int i=1; i<=n; i++)
+    {
+        if(i % 3 == 0)
+        {
+            printf("%d\n",i);        
+        }
+    }
+
+    return 0;
+}
+
+/*
+OUTPUT
+3
+6
+9
+12
+15
+18
+21
+24
+27
+30
+33
+36
+39
+42
+45
+48
+51
+54
+57
+60
+63
+66
+69
+72
+75
+78
+81
+84
+87
+90
+93
+96
+99
+*/
